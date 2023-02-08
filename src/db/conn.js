1eefe2e1c5e8de8e04c1d5ee506e558cb9490efe
mohd.habeeb <mohd.habeeb8123@gmail.com>
2023-02-08 19:16:49 +0530

    3

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1:27017/registration")
+// mongoose.connect("mongodb://127.0.0.1:27017/registration")
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/registration";
+mongoose.connect(MONGO_URI)
 .then(()=>{
-    console.log("connected successfully")
+    console.log(" connected to mongodb successfully")
 }).catch((err)=>{
     console.log(err)
 })
